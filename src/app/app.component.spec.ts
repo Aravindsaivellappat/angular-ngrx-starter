52f9a247231a3918604a13c0cce9a044fe8c59e8
Aravind Sai <aravindsai@hortari.in>
2019-10-21 17:58:34 +0530

    changed app title in test spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'guidelight'`, () => {
+  it(`should have as title 'angular-ngrx-starter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('guidelight');
+    expect(app.title).toEqual('angular-ngrx-starter');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to guidelight!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-ngrx-starter!');
   });
 });
